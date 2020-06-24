3c80a069e579d77ca320afe249af6f3cf10484ef
LuisGH1234 <u201516808@upc.edu.pe>
2020-06-23 21:17:04 -0500

    :hammer: authority url updated

--- lib/common/constants/api_urls.dart
+++ lib/common/constants/api_urls.dart
@@ -1,4 +1,5 @@
-const AUTHORITY_URI = "10.0.2.2:3000";
+// const AUTHORITY_URI = "127.0.0.1:3000";
+const AUTHORITY_URI = "movingperu.herokuapp.com";
 const BASE_PATH = "/api/v1";
 const USER_PATH = "$BASE_PATH/user";
 const AUTH_PATH = "$BASE_PATH/auth";
--- lib/presentation/pages/auth/login.dart
+++ lib/presentation/pages/auth/login.dart
@@ -37,7 +37,7 @@ class _LoginState extends State<Login> {
 
     return Scaffold(
       key: _scaffoldKey,
-      resizeToAvoidBottomInset: false,
+      // resizeToAvoidBottomInset: false,
       backgroundColor: $Colors.BACKGROUD,
       body: SafeArea(
         child: BlocConsumer<AuthBloc, AuthState>(listener: (context, state) {
@@ -46,91 +46,94 @@ class _LoginState extends State<Login> {
             showSnackbarError(state.loginEvent.message);
           }
         }, builder: (context, state) {
-          return Container(
-            // width: MediaQuery.of(context).size.width,
-            // height: MediaQuery.of(context).size.height,
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Flex(
-              direction: Axis.vertical,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              // mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top: 80),
-                  child: Logo(width: 250),
-                ),
-                Spacer(),
-                Input(
-                  onChanged: (value) {
-                    _username = value;
-                  },
-                  hintText: "Usuario",
-                  margin: EdgeInsets.only(bottom: 15),
-                ),
-                Input(
-                  onChanged: (value) {
-                    _password = value;
-                  },
-                  obscureText: true,
-                  hintText: "Contraseña",
-                  margin: EdgeInsets.only(bottom: 15),
-                ),
-                Button(
-                  "INICIAR SESIÓN",
-                  loading: state.loginEvent.loading,
-                  onPressed: () {
-                    // authBloc.authenticated();
-                    bool isEmail = RegExp(
-                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                        .hasMatch(_username);
-                    bool emptyField = _username.isEmpty || _password.isEmpty;
-                    if (isEmail && !emptyField) {
+          return SingleChildScrollView(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height -
+                  MediaQuery.of(context).padding.top,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 8 / 4,
+                    // padding: const EdgeInsets.only(top: 80),
+                    child: Logo(width: 250),
+                  ),
+                  Spacer(),
+                  Input(
+                    onChanged: (value) {
+                      _username = value;
+                    },
+                    hintText: "Usuario",
+                    margin: EdgeInsets.only(bottom: 15),
+                  ),
+                  Input(
+                    onChanged: (value) {
+                      _password = value;
+                    },
+                    obscureText: true,
+                    hintText: "Contraseña",
+                    margin: EdgeInsets.only(bottom: 15),
+                  ),
+                  Button(
+                    "INICIAR SESIÓN",
+                    loading: state.loginEvent.loading,
+                    onPressed: () {
                       // authBloc.authenticated();
-                      authBloc.login(_username, _password);
-                    } else if (emptyField)
-                      showSnackbarError("Debe llenar los campos");
-                    else
-                      showSnackbarError("El correo no es válido");
-                  },
-                ),
-                Center(
-                  child: FlatButton(
-                      onPressed: () {},
-                      child: Text(
-                        "¿Olvidaste tu contraseña?",
-                        style:
-                            TextStyle(color: Color(0xffED1C24), fontSize: 12),
-                      )),
-                ),
-                Spacer(),
-                Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "¿Deseas afiliarte como comercio?",
-                        style: TextStyle(
-                            color: Color(0xff000000),
-                            fontSize: 14,
-                            fontStyle: FontStyle.normal),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 10),
-                        child: FlatButton(
-                          onPressed: () {
-                            _navigateToRegister(context);
-                          },
-                          child: Text(
-                            "Registrate aqui",
-                            style: TextStyle(
-                                color: Color(0xffED1C24), fontSize: 12),
+                      bool isEmail = RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(_username);
+                      bool emptyField = _username.isEmpty || _password.isEmpty;
+                      if (isEmail && !emptyField) {
+                        // authBloc.authenticated();
+                        authBloc.login(_username, _password);
+                      } else if (emptyField)
+                        showSnackbarError("Debe llenar los campos");
+                      else
+                        showSnackbarError("El correo no es válido");
+                    },
+                  ),
+                  Center(
+                    child: FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          "¿Olvidaste tu contraseña?",
+                          style:
+                              TextStyle(color: Color(0xffED1C24), fontSize: 12),
+                        )),
+                  ),
+                  Spacer(),
+                  Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "¿Deseas afiliarte como comercio?",
+                          style: TextStyle(
+                              color: Color(0xff000000),
+                              fontSize: 14,
+                              fontStyle: FontStyle.normal),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 10),
+                          child: FlatButton(
+                            onPressed: () {
+                              _navigateToRegister(context);
+                            },
+                            child: Text(
+                              "Registrate aqui",
+                              style: TextStyle(
+                                  color: Color(0xffED1C24), fontSize: 12),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         }),
