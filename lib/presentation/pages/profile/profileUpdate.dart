77755456f1a46f17c503c79bcb354f8f4a41fa3e
Paul <palvareze7@gmail.com>
2020-07-02 23:24:14 -0500

    Get user id from preference on update user profile

--- lib/presentation/pages/profile/profileUpdate.dart
+++ lib/presentation/pages/profile/profileUpdate.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ubermove/common/constants/api_urls.dart';
 import 'package:ubermove/common/constants/colors.dart';
+import 'package:ubermove/domain/models/user.dart';
 import 'package:ubermove/network/core/api_manager.dart';
 import 'package:ubermove/presentation/blocs/auth/auth.bloc.dart';
 import 'package:ubermove/presentation/widgets/button.dart';
@@ -18,18 +20,19 @@ class ProfileUpdate extends StatelessWidget {
   String _phone = "";
   String _address = "";
 
-  Future commitUpdates(String names, lastnames, phone, address) async {
-    print(names + lastnames + phone + address);
+  Future commitUpdates(BuildContext context, String names, lastnames, phone, address) async {
+    final prefs = await SharedPreferences.getInstance();
+    var userID = await prefs.getInt('userID');
 
-    //TODO: Usage of preferences for request and changing user on prefs after success
     final path = "$USER_PATH/updateprofile";
-    final data = {"id": "1",'firstName': _names, 'lastName': _lastnames, 'phone': _phone};
+    final data = {"id": userID,'firstName': _names, 'lastName': _lastnames, 'phone': _phone};
+    Navigator.pop(context);
     return ApiManager.post(path, body: data, tokenRequired: true);
   }
 
   @override
   Widget build(BuildContext context) {
-    final authBloc = context.bloc<AuthBloc>();
+    final bloc = context.bloc<AuthBloc>();
 
     return Scaffold(
       appBar: AppBar(title: Text('Actualizar datos')),
@@ -84,7 +87,7 @@ class ProfileUpdate extends StatelessWidget {
                 padding: const EdgeInsets.only(top:35, bottom: 15),
                 child: Button(
                   "Actualizar",
-                  onPressed: () {commitUpdates(_names, _lastnames, _phone, _address);},
+                  onPressed: () {commitUpdates(context, _names, _lastnames, _phone, _address);},
                 ),
               )
             ],
